c07957e03b359707e2ebd96bc71ff62edc8855fc
hunteforsyth <hunter@hforsyth.com>
2015-01-30 23:48:02 -0500

    Made android app stuff

--- Android/Selfer/app/src/main/java/uofthacks2015/selfer/selfer/Tutorial.java
+++ Android/Selfer/app/src/main/java/uofthacks2015/selfer/selfer/Tutorial.java
@@ -2,8 +2,9 @@ package uofthacks2015.selfer.selfer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+
 
 
 public class Tutorial extends ActionBarActivity {
@@ -14,26 +15,9 @@ public class Tutorial extends ActionBarActivity {
         setContentView(R.layout.activity_tutorial);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
-        return true;
+    public void getStarted(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
